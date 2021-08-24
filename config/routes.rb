25e318a6db63257d119aa5f1f843337decc7c334
Romain Thiriot <85564151+Romain-Th@users.noreply.github.com>
2021-08-24 14:45:49 +0200

    MVP routes creation and some little more

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :products, only: [:index, :show ], shallow: true do
+    resources :feedbacks, only: [:new, :create, :show]
+  end
 end
+
+# (nester dans products) ressources :favorites, only: [:index, :create, :destroy]
+  # if Users.admin? = true can create / edit product
+
+# (nester products dans brands) -- ressources :brands do
+  # if Users.admin? = true can create / edit product
+  # ressources :products
+
+# search bar partout ?? pour avoir acces a la show de brand
