b97c13fb1c8728659f941bdf565a1bf8ce7afa89
robin chane <robin.chane@gmail.com>
2021-08-24 14:57:08 +0200

    added several validations

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -3,7 +3,7 @@ class Brand < ApplicationRecord
   has_many :products
 
   # validation
-  validates :name, presence: true, uniquess: { message: "this brand already exists" }
+  validates :name, presence: true, uniqueness: { message: "this brand already exists" }
   validates :description, presence: true,
                           length: { minimum: 15, message: "description must be at least 15 character-long" }
 end
