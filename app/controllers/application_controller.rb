90cc387c8da3521ec5d20c562df8803eaffa9af4
Romain Thiriot <85564151+Romain-Th@users.noreply.github.com>
2021-08-25 17:41:51 +0200

    changement devise username connection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,6 @@ class ApplicationController < ActionController::Base
   private
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email])
   end
 end
