e8a33b38a7af392d53463a25c57cd79f2771b05e
Romain Thiriot <85564151+Romain-Th@users.noreply.github.com>
2021-08-27 14:42:49 +0200

    debug test remove taregt form JS

--- app/javascript/controllers/insert_in_list_controller.js
+++ app/javascript/controllers/insert_in_list_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "stimulus";
 
 export default class extends Controller {
-  static targets = ['items', 'form'];
+  static targets = ['items'];
 
   connect() {
     console.log(this.element);
