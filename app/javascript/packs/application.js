1148be817dbae602fda0c12e027d90abf2388c44
Romain Thiriot <85564151+Romain-Th@users.noreply.github.com>
2021-08-27 15:25:28 +0200

    cloudinary debug for heroku

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -27,10 +27,9 @@ import "bootstrap";
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
+import "controllers"
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
 });
-
-import "controllers"
