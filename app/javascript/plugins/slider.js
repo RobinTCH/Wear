4cf66b613552e877e3358ed1ceb11a10d5ccce6f
robin chane <robin.chane@gmail.com>
2021-08-31 15:06:47 +0200

    not finished slider indicator

--- app/javascript/plugins/slider.js
+++ app/javascript/plugins/slider.js
@@ -1,22 +1,33 @@
 const sliderValue = document.getElementById("slider-value");
 
+function hideValue() {
+  sliderValue.style.opacity = "0";
+  sliderValue.style.transition = "opacity 0.4s";
+}
+
 function initSlider() {
   const range = document.getElementById("products_filter_average_rating");
-  range.addEventListener('mousemove', (event) => {
-    sliderValue.innerHTML = range.value;
-    const sliderDiv = document.querySelector(".filter-rating");
-    sliderValue.style.marginLeft = `${range.value * (sliderDiv.clientWidth - 22) / 100}px`;
-    sliderValue.style.visibility = "visible";
+  range.addEventListener('click', (event) => {
+    range.addEventListener('mousemove', (event) => {
+      sliderValue.innerHTML = range.value;
+      const sliderDiv = document.querySelector(".filter-rating");
+      sliderValue.style.marginLeft = `${range.value * (sliderDiv.clientWidth - 22) / 100}px`;
+      sliderValue.style.opacity = "1";
+    });
   });
 
   range.addEventListener('keydown', (event) => {
-    document.getElementById("slider-value").innerHTML = range.value
+    document.getElementById("slider-value").innerHTML = "<span>" + range.value + "</span>"
   });
 
   range.addEventListener('touchend', (event) => {
-    document.getElementById("slider-value").innerHTML = range.value
-    sliderValue.style.visibility = "hidden"
+    document.getElementById("slider-value").innerHTML = "<span>" + range.value + "</span>"
+  });
+
+  range.addEventListener('mouseup', (event) => {
+    hideValue()
   });
 }
+
 
 export { initSlider };
